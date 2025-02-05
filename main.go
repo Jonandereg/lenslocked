7932748b79fcc0dd711d137e7a1848a60943dbf4
Jonander <jonander.eg@gmail.com>
2025-02-05 22:18:35 +0100

    implement path handler with 404 error handling

--- main.go
+++ main.go
@@ -15,9 +15,26 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>If you have any issues please reach out at <a href=\"mailto:random@email.com\">random@email.com</a></p>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>page not found!</h1>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		notFoundHandler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
